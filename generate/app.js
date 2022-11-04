2bfea359fbfa8a1b6a021a629abf585b42947370
Myles Nieman <MSNieman01@indianatech.net>
2022-11-04 11:41:23 -0400

    "finish adding /ksql"

--- generate/app.js
+++ generate/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var ksqlRouter = require('./routes/ksql');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/ksql', ksqlRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
